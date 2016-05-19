42ccbd9d75f3e8de60f8ae1a374842a67d96c3f9
nehiljain <jain.nehil@gmail.com>
2016-05-19 12:10:15 -0500

    removing 0 min items by using cieling instead of floor)

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -1,6 +1,6 @@
 var User = require('../models/User');
 
-var Article = require('../models/Article')
+var Article = require('../models/Article');
 
 
 
@@ -12,21 +12,21 @@ exports.index = function(req,res,next){
 	    	user.tokens.forEach( function(item){
 	    		if ( item.kind == 'pocket' ){
 	    			pocketConnected = true;
-	    			accessToken = item.accessToken
+	    			accessToken = item.accessToken;
 	    		}
-	    	})
+	    	});
 
 	    	Article.find({}).where({'email':req.user.email,'status':"0"}).exec( function(err,articles){
 	    		var timeRequired = 0;
 	    		if ( !articles.length ){
-	    			articles = []
+	    			articles = [];
 	    		} else {
 
 	    			articles.forEach( function(item){
 	    				item.final_title = item.resolved_title || item.given_title ||  item.resolved_url || 'Article';
-	    				item.estimatedTime = Math.floor(item.word_count/ 200) 
-	    				timeRequired += item.estimatedTime
-	    			})
+	    				item.estimatedTime = Math.ceil(item.word_count/ 200);
+	    				timeRequired += item.estimatedTime;
+	    			});
 	    		}
 
 	    		res.render('dashboard',{
@@ -36,10 +36,10 @@ exports.index = function(req,res,next){
 		  			articleCount : articles.length,
 		  			timeRequired : timeRequired,
 		  			articles : articles
-			    })
-	    	})
+			    });
+	    	});
 	    });
 	} else {
 		res.redirect('/');
 	}
-}
+};
