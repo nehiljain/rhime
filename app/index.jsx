6d38cf8e1749e78d6b28eb5eccd80a61b0c6a681
Selvam Palanimalai <selvam.palanimalai@athletigen.com>
2016-07-02 14:31:23 -0300

    bugfix

--- app/index.jsx
+++ app/index.jsx
@@ -2,7 +2,8 @@ var React = require( 'react');
 var ReactDOM  = require('react-dom');
 var App = require('./components/dashboard/Dashboard.jsx');
 
-var initialState = JSON.parse(document.getElementById('initial-state').innerHTML);
+var state = document.getElementById('initial-state').innerHTML.trim() || "{}";
+var initialState = JSON.parse(state);
 
 if( document.getElementById('dashboard') ){
 	ReactDOM.render(<App dashboardState={initialState}/>, document.getElementById('dashboard') );	
