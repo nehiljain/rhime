f9558d8e61583dc959d7d3fecc141161f165cfd8
Selvam Palanimalai <selvam.palanimalai@athletigen.com>
2016-09-13 12:40:43 -0700

    forcing port to 3000

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ mongoose.connection.on('error', function() {
 /**
  * Express configuration.
  */
-app.set('port', process.env.PORT || 3000);
+app.set('port',3000);
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
